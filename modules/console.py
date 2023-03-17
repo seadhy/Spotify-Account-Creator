2173680a02b755975da20db2f8aff747ce1a6b2a
Susan Bhattarai <83157048+ImmaSB@users.noreply.github.com>
2023-03-17 07:50:50 +0545

    Update console.py
    Added support for linux systems for the title changer

--- modules/console.py
+++ modules/console.py
@@ -3,11 +3,13 @@ from colorama import Fore
 from threading import Lock, active_count
 from time import sleep, perf_counter
 from datetime import datetime
-from ctypes import windll
 from pystyle import Center, Colors, Colorate
+import sys
 
 lock = Lock()
 
+if sys.platform.startswith('win'):
+    from ctypes import windll
 
 class Console:
     created = 0
@@ -22,27 +24,30 @@ class Console:
     @staticmethod
     def printe(content: str):
         lock.acquire()
-        print(f"{Fore.LIGHTBLACK_EX}[{Fore.LIGHTWHITE_EX}{datetime.strftime(datetime.now(), '%X').replace(':', f'{Fore.LIGHTBLACK_EX}:{Fore.LIGHTWHITE_EX}')}{Fore.LIGHTBLACK_EX}] {Fore.LIGHTBLACK_EX}[{Fore.LIGHTRED_EX}Error Occurred{Fore.LIGHTBLACK_EX}]{Fore.LIGHTWHITE_EX} > {content}")
+        print(
+            f"{Fore.LIGHTBLACK_EX}[{Fore.LIGHTWHITE_EX}{datetime.strftime(datetime.now(), '%X').replace(':', f'{Fore.LIGHTBLACK_EX}:{Fore.LIGHTWHITE_EX}')}{Fore.LIGHTBLACK_EX}] {Fore.LIGHTBLACK_EX}[{Fore.LIGHTRED_EX}Error Occurred{Fore.LIGHTBLACK_EX}]{Fore.LIGHTWHITE_EX} > {content}")
         lock.release()
 
     @staticmethod
     def printi(content: str):
         lock.acquire()
-        print(f"{Fore.LIGHTBLACK_EX}[{Fore.LIGHTWHITE_EX}{datetime.strftime(datetime.now(), '%X').replace(':', f'{Fore.LIGHTBLACK_EX}:{Fore.LIGHTWHITE_EX}')}{Fore.LIGHTBLACK_EX}] {Fore.LIGHTBLACK_EX}[{Fore.LIGHTYELLOW_EX}Info{Fore.LIGHTBLACK_EX}]{Fore.LIGHTWHITE_EX} > {content}")
+        print(
+            f"{Fore.LIGHTBLACK_EX}[{Fore.LIGHTWHITE_EX}{datetime.strftime(datetime.now(), '%X').replace(':', f'{Fore.LIGHTBLACK_EX}:{Fore.LIGHTWHITE_EX}')}{Fore.LIGHTBLACK_EX}] {Fore.LIGHTBLACK_EX}[{Fore.LIGHTYELLOW_EX}Info{Fore.LIGHTBLACK_EX}]{Fore.LIGHTWHITE_EX} > {content}")
         lock.release()
 
     @staticmethod
-    def printmf(content: str):
+    def printm(content: str):
         lock.acquire()
-        print(f"{Fore.LIGHTBLACK_EX}[{Fore.LIGHTWHITE_EX}{datetime.strftime(datetime.now(), '%X').replace(':', f'{Fore.LIGHTBLACK_EX}:{Fore.LIGHTWHITE_EX}')}{Fore.LIGHTBLACK_EX}] {Fore.LIGHTBLACK_EX}[{Fore.LIGHTBLUE_EX}Mail Verified{Fore.LIGHTBLACK_EX}]{Fore.LIGHTWHITE_EX} > {content}")
+        print(
+            f"{Fore.LIGHTBLACK_EX}[{Fore.LIGHTWHITE_EX}{datetime.strftime(datetime.now(), '%X').replace(':', f'{Fore.LIGHTBLACK_EX}:{Fore.LIGHTWHITE_EX}')}{Fore.LIGHTBLACK_EX}] {Fore.LIGHTBLACK_EX}[{Fore.LIGHTBLUE_EX}Mail Verified{Fore.LIGHTBLACK_EX}]{Fore.LIGHTWHITE_EX} > {content}")
         lock.release()
 
     @staticmethod
     def printhc(content: str):
         lock.acquire()
-        print(f"{Fore.LIGHTBLACK_EX}[{Fore.LIGHTWHITE_EX}{datetime.strftime(datetime.now(), '%X').replace(':', f'{Fore.LIGHTBLACK_EX}:{Fore.LIGHTWHITE_EX}')}{Fore.LIGHTBLACK_EX}] {Fore.LIGHTBLACK_EX}[{Fore.LIGHTMAGENTA_EX}Humanization Completed{Fore.LIGHTBLACK_EX}]{Fore.LIGHTWHITE_EX} > {content}")
+        print(
+            f"{Fore.LIGHTBLACK_EX}[{Fore.LIGHTWHITE_EX}{datetime.strftime(datetime.now(), '%X').replace(':', f'{Fore.LIGHTBLACK_EX}:{Fore.LIGHTWHITE_EX}')}{Fore.LIGHTBLACK_EX}] {Fore.LIGHTBLACK_EX}[{Fore.LIGHTMAGENTA_EX}Humanization Completed{Fore.LIGHTBLACK_EX}]{Fore.LIGHTWHITE_EX} > {content}")
         lock.release()
-
 
     @staticmethod
     def printtc(content: str):
@@ -55,8 +60,16 @@ class Console:
 class Tools:
 
     @staticmethod
+    def set_terminal_title(title: str):
+        if sys.platform.startswith('win'):
+            ctypes.windll.kernel32.SetConsoleTitleW(title)
+        else:
+            sys.stdout.write(f"\x1b]2;{title}\x07")
+            sys.stdout.flush()
+
+    @staticmethod
     def titleChanger(use_target: str, target_to: int):
-        windll.kernel32.SetConsoleTitleW('Initializing...')
+        Tools.set_terminal_title('Initializing...')
 
         starting_time = perf_counter()
         while True:
@@ -65,15 +78,15 @@ class Tools:
             if use_target == 'y':
                 remaining = target_to - Console.created
                 if remaining > 0:
-                    windll.kernel32.SetConsoleTitleW(f"Seasmash Spotify Creator  |  Threads: {active_count() - 2}  |  Created: {Console.created}  |  Speed: {created_min}/m  |  Remaining: {remaining}  |  Elapsed: {round(perf_counter() - starting_time, 1)}s  |  Made by github.com/seadhy")
+                    Tools.set_terminal_title(f"Seasmash Spotify Creator  |  Threads: {active_count() - 2}  |  Created: {Console.created}  |  Speed: {created_min}/m  |  Remaining: {remaining}  |  Elapsed: {round(perf_counter() - starting_time, 1)}s  |  Made by github.com/seadhy")
                 else:
-                    windll.kernel32.SetConsoleTitleW(f"Seasmash Spotify Creator  |  Threads: {active_count() - 2}  |  Created: {Console.created}  |  Speed: {created_min}/m  |  Remaining: {remaining}  |  Elapsed: {round(perf_counter() - starting_time, 1)}s  |  Made by github.com/seadhy")
+                    Tools.set_terminal_title(f"Seasmash Spotify Creator  |  Threads: {active_count() - 2}  |  Created: {Console.created}  |  Speed: {created_min}/m  |  Remaining: {remaining}  |  Elapsed: {round(perf_counter() - starting_time, 1)}s  |  Made by github.com/seadhy")
                     sleep(5)
                     Console.printtc(threading.current_thread().name + " is closed. The program will close itself in 3 seconds...")
                     sleep(3)
                     exit()
             else:
-                windll.kernel32.SetConsoleTitleW(f"Seasmash Spotify Creator  |  Threads: {active_count() - 2}  |  Created: {Console.created}  |  Speed: {created_min}/m  |  Elapsed: {round(perf_counter() - starting_time, 1)}s  |  Made by github.com/seadhy")
+                Tools.set_terminal_title(f"Seasmash Spotify Creator  |  Threads: {active_count() - 2}  |  Created: {Console.created}  |  Speed: {created_min}/m  |  Elapsed: {round(perf_counter() - starting_time, 1)}s  |  Made by github.com/seadhy")
 
     @staticmethod
     def printLogo():
